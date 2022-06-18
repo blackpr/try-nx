2bbda3448c8da30897325177a976c39e44ed6ffc
Tim <blackpr.tim@gmail.com>
2022-06-18 19:34:27 +0300

    scaffold routing

--- apps/moviesdb/src/app/app.module.ts
+++ apps/moviesdb/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreDataModule } from '@tim/core-data';
 import { CoreStateModule } from '@tim/core-state';
 import { MaterialModule } from '@tim/material';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -16,6 +17,7 @@ import { MaterialModule } from '@tim/material';
     CoreDataModule,
     CoreStateModule,
     MaterialModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
