06f51e962cfe9b02cbee172df1f800824e9f4da7
Tim <blackpr.tim@gmail.com>
2022-06-18 18:57:38 +0300

    scaffold core-data lib angular module

--- apps/moviesdb/src/app/app.module.ts
+++ apps/moviesdb/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreDataModule, coreDataRoutes } from '@tim/core-data';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreDataModule],
   providers: [],
   bootstrap: [AppComponent],
 })
