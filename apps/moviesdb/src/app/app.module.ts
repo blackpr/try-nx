0637b9b5f93ee32d01033a90a64c0b8e548f1b85
Tim <blackpr.tim@gmail.com>
2022-06-18 19:04:40 +0300

    scaffold core-state angular lib

--- apps/moviesdb/src/app/app.module.ts
+++ apps/moviesdb/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CoreDataModule, coreDataRoutes } from '@tim/core-data';
+import { CoreDataModule } from '@tim/core-data';
+import { CoreStateModule } from '@tim/core-state';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreDataModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreDataModule,
+    CoreStateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
